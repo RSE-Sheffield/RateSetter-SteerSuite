55c734ae4c11f38132997995e9e1c1609fa9205d
John Charlton <coolmule0@hotmail.co.uk>
2022-03-10 16:10:03 +0000

    agent gen

--- testcases/PR/gen.py
+++ testcases/PR/gen.py
@@ -1,4 +1,6 @@
 from typing import List, Set, TypedDict
+import math
+import random
 import xml.etree.ElementTree as ET
 # import numpy as np
 
@@ -173,12 +175,150 @@ class GenTestcase():
 			if level and (not elem.tail or not elem.tail.strip()):
 				elem.tail = i
 
+	def add_goal(self, goal_sequence_xml, goal_dict):
+		"""
+		Add a goal to an agent xml
+		"""
+		goal_type = goal_dict["goal_type"]
+		
+		if(goal_type == "statictarget"):
+			goal_location = goal_dict["goal_location"]
+			seekStaticTarget = ET.SubElement(goal_sequence_xml, 'seekStaticTarget')
+			targetLocation = ET.SubElement(seekStaticTarget, 'targetLocation')
+			ET.SubElement(targetLocation, 'x').text= str(goal_location[0])
+			ET.SubElement(targetLocation, 'y').text= "0"
+			ET.SubElement(targetLocation, 'z').text= str(goal_location[1])
+			ET.SubElement(seekStaticTarget, 'timeDuration').text = "1000.0"
+			ET.SubElement(seekStaticTarget, 'desiredSpeed').text = "1.3"
+		
+		elif(goal_type == "boxregion"):
+			goal_region = goal_dict["goal_region"]
+			goal_location = [0,0]
+			seekAxisAlignedBoxRegion = ET.SubElement(goal_sequence_xml, 'seekAxisAlignedBoxRegion')
+			targetLocation = ET.SubElement(seekAxisAlignedBoxRegion, 'targetLocation')
+			ET.SubElement(targetLocation, 'x').text= str(goal_location[0])
+			ET.SubElement(targetLocation, 'y').text= "0"
+			ET.SubElement(targetLocation, 'z').text= str(goal_location[1])
+			ET.SubElement(seekAxisAlignedBoxRegion, 'desiredSpeed').text = "1.3"
+			ET.SubElement(seekAxisAlignedBoxRegion, 'timeDuration').text = "1000.0"
+			goalRegionBounds = ET.SubElement(seekAxisAlignedBoxRegion, 'goalRegionBounds')
+			ET.SubElement(goalRegionBounds, 'xmin').text= str(goal_region[0])
+			ET.SubElement(goalRegionBounds, 'xmax').text= str(goal_region[1])
+			ET.SubElement(goalRegionBounds, 'ymin').text= "0"
+			ET.SubElement(goalRegionBounds, 'ymax').text= "0"
+			ET.SubElement(goalRegionBounds, 'zmin').text= str(goal_region[2])
+			ET.SubElement(goalRegionBounds, 'zmax').text= str(goal_region[3])
+		
+		elif(goal_type == "targetSet"):
+			seekTargetSet = ET.SubElement(goal_sequence_xml, 'seekStaticTargetSet')
+			targetLocationsSet = ET.SubElement(seekTargetSet, 'targetLocationsSet')
+			for goal in goal_dict["goal_locations"]:
+				targetLocation = ET.SubElement(targetLocationsSet, 'targetLocation')
+				ET.SubElement(targetLocation, 'x').text = str(goal[0])		
+				ET.SubElement(targetLocation, 'y').text = str(goal[1])
+				ET.SubElement(targetLocation, 'z').text = str(goal[2])
 
+			ET.SubElement(seekTargetSet, 'timeDuration').text = "1000.0"
+			ET.SubElement(seekTargetSet, 'desiredSpeed').text = "1.3"
+			if("parameters" in goal_dict):
+				Behaviour = ET.SubElement(seekTargetSet, 'Behaviour')
+				Parameters = ET.SubElement(Behaviour, 'Parameters')
+				for spec in goal_dict["parameters"]:
+					lp = ET.SubElement(Parameters, spec.replace(" ", ""))
+					ET.SubElement(lp, 'key').text = spec
+					ET.SubElement(lp, 'value').text = "1"
 
-	def spawn_agents_in_square(self, square: List[float], num_agents: int, **agent_behaviour):
+		else:
+			print("unknown goal type attempting to be added")
+
+	def make_agent(self, location: List[float], agent_behavior: AgentInfo):
+		"""
+		"""
+		agent= ET.SubElement(self.xmlparent, 'agent')
+
+		radius = agent_behavior["radius"]
+		sdradius = agent_behavior["sdradius"]
+		goals_arr = agent_behavior["goals"]
+
+		#initial conditions
+		initialConditions= ET.SubElement(agent, 'initialConditions')
+		direction = ET.SubElement(initialConditions, 'direction')
+		ET.SubElement(direction, 'x').text = "0"
+		ET.SubElement(direction, 'y').text = "0"
+		ET.SubElement(direction, 'z').text = "0"
+		position = ET.SubElement(initialConditions, 'position')
+		ET.SubElement(position, 'x').text = str(location[0])
+		ET.SubElement(position, 'y').text = "0"
+		ET.SubElement(position, 'z').text = str(location[1])
+		ET.SubElement(initialConditions, 'radius').text= str(radius)
+		ET.SubElement(initialConditions, 'sdradius').text= str(sdradius)
+		ET.SubElement(initialConditions, 'speed').text= "0"
+
+		#goal sequence
+		goalSequence= ET.SubElement(agent, 'goalSequence')
+		for g in goals_arr:
+			self.add_goal(goalSequence, g)
+
+		# add agent behaviour
+		# behaviour = ET.SubElement(agent, 'behaviour')
+		# sdradius_z = ET.SubElement(behaviour, 'sdradius_z')
+		# ET.SubElement(sdradius_z, 'z0').text= "-3"
+		# ET.SubElement(sdradius_z, 'z1').text= "0"
+		# ET.SubElement(sdradius_z, 'sd0').text= "0.2"
+		# ET.SubElement(sdradius_z, 'sd1').text= "0"
+
+	def spawn_agents_in_square(self, square: List[float], num_agents: int, **agent_behaviour: AgentInfo) -> int:
 		"""
 		spawn some agents with a square region. It assumes the whole area is free from other object, and agents can spawn anywhere within the square
+
+		square: [x1,z1,  x2, z2] 2 corners of the square (with min x,z in one and max x,z in the other)
+		
+		Returns: the number of agents that did not fin into the spawned region
 		"""
+		radius = agent_behaviour["radius"]
+		sdradius = agent_behaviour["sdradius"]
+		lengths = [
+			square[2] - square[0],
+			square[3] - square[1]
+		]
+		origin = [square[0], square[1]]
+		goals = agent_behaviour["goals"]
+
+		# Can we fit all the agents?
+		nums_per_side = [math.floor(abs(lengths[0]/((radius+sdradius)*2))), 
+						 math.floor(abs(lengths[1]/((radius+sdradius)*2)))]
+		total_num = int(nums_per_side[0] * nums_per_side[1])
+		leftover = num_agents - total_num
+		num_agents = total_num if leftover > 0 else num_agents
+
+		# calculate the boxes available
+		box_lengths = [lengths[0] / nums_per_side[0], lengths[1] / nums_per_side[1]]
+		box_occupancy = list(range(0,total_num))
+		box_size = box_lengths
+		box_center = [box_size[0] / 2, box_size[1] / 2]
+		# extra_space = np.abs(box_size - (2*(radius+sdradius))) / 2
+
+		for i in range(num_agents):
+			# select a free box
+			boxId = random.choice(box_occupancy)
+
+			# choose some space in the box
+			offset_range = 0.0
+			x_off = random.uniform(0, offset_range) * 2 - offset_range
+			y_off = random.uniform(0, offset_range) * 2 - offset_range
+
+			# convert boxId to real location in x-y and create
+			row_id = math.floor(boxId / nums_per_side[0])
+			colum_id = boxId % nums_per_side[0]
+			col_loc = origin[0] + (colum_id * box_size[0]) + box_center[0] + x_off#+ (-extra_space[1] + random.uniform(0,2) * extra_space[1])
+			row_loc = origin[1] + (row_id   * box_size[1]) + box_center[1] + y_off#+ (-extra_space[0] + random.uniform(0,2) * extra_space[0])
+			self.make_agent([col_loc, row_loc], agent_behaviour)
+			# self.make_agent(radius, sdradius, [col_loc, row_loc], goals)
+
+			# Set id as occupied
+			box_occupancy.remove(boxId)
+
+		return leftover if leftover > 0 else 0
 
 
 train: TrainInfo = {
@@ -189,15 +329,20 @@ train: TrainInfo = {
 	"door_size": 1
 }
 
+gb: AgentGoalBox = {
+	"goal_type": "boxregion",
+	"target_location": [0,0], # positions
+	"goal_region": [0,0,5,5], # (x1,y1, x2,y2) 2 opposite corners of the box goal
+}
+
 agent_spec: AgentInfo = {
 	"radius": 0.3, # physical radius of person
 	"sdradius": 0, # social distancing radius
-	"goals": list[AgentGoal],
-
+	"goals": [gb]
 }
 
 tc = GenTestcase()
 tc.gen_train(**train)
-tc.spawn_agents_in_square([0,0,5,5], 5, **AgentInfo)
+tc.spawn_agents_in_square([0,0,5,5], 5, **agent_spec)
 print(tc.dump())
 tc.write("testcases/PR/foo.xml")
